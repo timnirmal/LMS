154186818fa64a9047425517e3f8429b9f9d1626
timnirmal <timnirmal@gmail.com>
2022-04-14 21:15:50 +0530

    contentlayer

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,21 @@
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import 'tailwindcss/tailwind.css'
 import '../styles/globals.css'
+//import '../css/prism.css'
+//import 'katex/dist/katex.css'
 
 import React from 'react'
 import type, {AppProps} from 'next/app'
 import Head from 'next/head'
 import {DefaultSeo} from 'next-seo'
+import { ThemeProvider } from 'next-themes'
 
 import SEO from '../next-seo.config'
 import {MessageProvider} from '../lib/message'
 import {AuthProvider} from "../lib/auth";
+
+import siteMetadata from '../data/siteMetadata'
+import LayoutWrapper from '../components/LayoutWrapper'
 
 function MyApp({Component, pageProps}: AppProps) {
     const pageMeta = (Component as any)?.defaultProps?.meta || {}
